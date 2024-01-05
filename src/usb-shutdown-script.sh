14a9e825d14a2ff7463abb755d4247462a251412
eniskastrati <enis@smartmile.eu>
2024-01-05 16:24:56 +0100

    updated script to get data from configuraton file

--- src/usb-shutdown-script.sh
+++ src/usb-shutdown-script.sh
@@ -2,17 +2,16 @@
 
 # Turns on the Script, if this option is set to false the script will not analyze the USBs.
 USB_CHECK_ENABLED=true
-TRUSTED_USB_LIST="USB ID'S";
-# IFS=',' read -ra CURRENT_CONNECTED_USB <<< `lsusb | sed -e 's/.*ID \([a-f0-9]\+:[a-f0-9]\+\).*/\1/g'`
+TRUSTED_USB_LIST=`cat ~/.config/usb.security/.config`;
 
 # Function to check for unknown USB devices and shut down if conditions are not met
 # If a USB device that is NOT in the list connects, then it will shut down.
 check_unknown_devices() {
-        if  lsusb | grep -q -v ${TRUSTED_USB_LIST}; then
-            echo "** Unknown USB device connected, shutting down... **"
-             /sbin/shutdown -h now
-        else
-            echo "** Currently all USB's are trusted! **";
+    if  lsusb | grep -q -v ${TRUSTED_USB_LIST}; then
+        echo "** Unknown USB device connected, shutting down... **"
+         /sbin/shutdown -h now
+    else
+        echo "** Currently all USB's are trusted! **";
     fi
 }
 
