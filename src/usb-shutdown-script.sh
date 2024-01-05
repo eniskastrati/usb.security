a4e0684df322c8df24a654eb0b41309fff05ffc0
eniskastrati <enis@smartmile.eu>
2024-01-05 14:33:50 +0100

    updating the script after debugging

--- src/usb-shutdown-script.sh
+++ src/usb-shutdown-script.sh
@@ -2,13 +2,17 @@
 
 # Turns on the Script, if this option is set to false the script will not analyze the USBs.
 USB_CHECK_ENABLED=true
+TRUSTED_USB_LIST="USB ID'S";
+# IFS=',' read -ra CURRENT_CONNECTED_USB <<< `lsusb | sed -e 's/.*ID \([a-f0-9]\+:[a-f0-9]\+\).*/\1/g'`
 
 # Function to check for unknown USB devices and shut down if conditions are not met
 # If a USB device that is NOT in the list connects, then it will shut down.
 check_unknown_devices() {
-    if lsusb | grep -q -v "1f75:0917\|054c:0243"; then
-        echo "Unknown USB device connected, shutting down..."
-        /sbin/shutdown -h now
+        if  lsusb | grep -q -v ${TRUSTED_USB_LIST}; then
+            echo "** Unknown USB device connected, shutting down... **"
+             /sbin/shutdown -h now
+        else
+            echo "** Currently all USB's are trusted! **";
     fi
 }
 
@@ -18,5 +22,5 @@ while true; do
     else
         echo "USB device check is disabled. Allowing connection..."
     fi
-    sleep 10 # Check every 10 seconds recommendet(change if you want)
+    sleep 2 # Check every 2 seconds recommendet(change if you want)
 done
